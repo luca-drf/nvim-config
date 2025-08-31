a38336c3812771d07633a6090cc867be8c14bcaf
Luca Da Rin Fioretto <ldarinfioretto@gmail.com>
2025-08-31 16:56:03 +0100

    Navigate autocomplete menus with Tab/S-Tab

--- init.lua
+++ init.lua
@@ -73,9 +73,30 @@ end)
 later(function() require('mini.basics').setup() end)
 later(function() require('mini.git').setup() end)
 later(function() require('mini.diff').setup() end)
-later(function() require('mini.completion').setup() end)
-later(function() require('mini.pairs').setup() end)
 later(function() require('mini.surround').setup() end)
+later(function() require('mini.pairs').setup() end)
+later(function()
+  require('mini.completion').setup()
+
+  local imap_expr = function(lhs, rhs)
+    vim.keymap.set('i', lhs, rhs, { expr = true })
+  end
+  imap_expr('<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
+  imap_expr('<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
+
+  _G.cr_action = function()
+    -- If there is selected item in popup, accept it with <C-y>
+    if vim.fn.complete_info()['selected'] ~= -1 then return '\25' end
+    -- Fall back to plain `<CR>`. You might want to customize according
+    -- to other plugins. For example if 'mini.pairs' is set up, replace
+    -- next line with `return MiniPairs.cr()`
+    -- return '\r'
+    return MiniPairs.cr()
+  end
+
+  vim.keymap.set('i', '<CR>', 'v:lua.cr_action()', { expr = true })
+end)
+
 later(function() require('mini.comment').setup() end)
 later(function() require('mini.trailspace').setup() end)
 later(function()
